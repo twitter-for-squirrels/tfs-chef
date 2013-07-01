f2cbaf55dd4b9baf65cc7ecb43280447c65445ad
David Bernal <david@davidthomasbernal.com>
2013-06-30 23:30:46 -0400

    Work on tfs app cookbook

--- cookbooks/tfs-app/recipes/default.rb
+++ cookbooks/tfs-app/recipes/default.rb
@@ -8,13 +8,43 @@
 #
 #
 
+secrets = Chef::EncryptedDataBagItem.load("secrets", "mysql")
+
+MYSQL_USER = "debian"
+
 deploy_branch "/var/www/tfs" do
+	action :force_deploy
+
 	repo "https://github.com/twitter-for-squirrels/twitter-for-squirrels.git"
 	enable_submodules true
-	branch "master"
+	branch "develop"
+	shallow_clone false
+
+	if node.roles.include? "db"
+		mysql_server = node
+	else
+		mysql_server = search(:node, "role:db").first
+	end
+
+	before_migrate do
+
+		template "#{release_path}/application/config/database.php" do
+			source "database.php.erb"
+			variables({
+				:mysql_host => mysql_server["ipaddress"],
+				:mysql_user => MYSQL_USER,
+				:mysql_password => secrets[node.chef_environment][MYSQL_USER],
+			})
+		end
+	end
 
 	migrate true
-	migrate_command "minion migrations::run"
+	migration_command "./minion migrations::run"
 
-	notifies :restart, "service[apache]"
+	purge_before_symlink([])
+  create_dirs_before_symlink([])
+  symlinks({})
+  symlink_before_migrate({})
+
+	notifies :restart, "service[apache2]"
 end
